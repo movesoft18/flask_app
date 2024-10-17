3a4dad482f3cf6238b973a724dafa45a3193867c
movesoft <movesoft@yandex.ru>
2024-10-17 14:51:45 +0400

    add alembic

--- app_data/definitions.py
+++ app_data/definitions.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase): pass
 
-mysql_user = 'user'
-mysql_passw = 'Aqua_DB04'
+#mysql_user = 'user'
+#mysql_passw = 'Aqua_DB04'
+mysql_user = 'root'
+mysql_passw = '123'
 sqlalchemy_mysql_connection_string = f"mysql+pymysql://{mysql_user}:{mysql_passw}@localhost/aqua_db?charset=utf8"
 mysql_connection = create_engine(sqlalchemy_mysql_connection_string)
